eae0531f4abd1c3b91de3ff25febaaf18b12fa25
Huzaifa <huzaifaansari294@gmail.com>
2024-09-06 00:05:20 +0530

    initial commit

---
+++ Module 2/Practiseset/tempCodeRunnerFile.py
@@ -0,0 +1,31 @@
+# Printing right-angle trianle with * using while loop
+
+# i = 1
+# while i <= 5:
+#     j = 1
+#     while j <= i:
+#         print('*', end ="")
+#         j = j + 1
+#     print()
+#     i = i + 1
+
+# # Printing right angle trinangle with 1, 12, 123, 1234, 12345 using while loop
+
+# i = 1
+# while i <=5:
+#     j = 1
+#     while j  <= i:
+#         print(j, end="")
+#         j = j+1
+#     print()
+#     i = i+1
+
+# # Printing right angle trinangle with 1, 22, 333, 4444, 5555 using while loop
+# i = 1
+# while i <= 5:
+#     j = 1
+#     while j <= i:
+#         print(i, end='')
+#         j = j + 1
+#     print()
+#     i = i + 1
---
+++ Module 2/Practiseset/test1.py
@@ -0,0 +1,57 @@
+# # Printing right-angle trianle with * using while loop
+
+# i = 1
+# while i <= 5:
+#     j = 1
+#     while j <= i:
+#         print('*', end ="")
+#         j = j + 1
+#     print()
+#     i = i + 1
+
+# # Printing right angle trinangle with 1, 12, 123, 1234, 12345 using while loop
+
+# i = 1
+# while i <=5:
+#     j = 1
+#     while j  <= i:
+#         print(j, end="")
+#         j = j+1
+#     print()
+#     i = i+1
+
+# # Printing right angle trinangle with 1, 22, 333, 4444, 5555 using while loop
+# i = 1
+# while i <= 5:
+#     j = 1
+#     while j <= i:
+#         print(i, end='')
+#         j = j + 1
+#     print()
+#     i = i + 1
+
+# # printing right angle triangle with * using FOR LOOP
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*", end="")
+#     print()
+
+# for i in range(10):
+#     for j in range(i+1):
+#         print(j, end= "")
+#     print()
+
+# for x in range(5):
+#     for y in range(x+1):
+#         print(x, end="")
+#     print()
+
+
+# Printing solid square using for loop
+num = 5
+for i in range(5):
+    print("*  " * num)
+
+
+
